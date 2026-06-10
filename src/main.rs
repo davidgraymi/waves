a622295679930800c85bc6fbf71e72a44fb8aad4
David Gray <davidgraymi@gmail.com>
2026-06-09 19:20:17 -0500

    add purple screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use nannou::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(PURPLE);
+    draw.to_frame(app, &frame).unwrap();
 }
